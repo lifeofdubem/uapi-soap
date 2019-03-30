17107f22af769d443800299441676368f95d353b
Dubem <lifeofdubem@gmail.com>
2019-03-30 10:47:40 +0300

    Add lowfarePaser test

--- test/services/air/parsers/lowfare-parser.test.js
+++ test/services/air/parsers/lowfare-parser.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 const chai = require('chai');
 
 const lowfareParser = require('../../../../src/services/air/parsers/lowfare-parser');
@@ -9,6 +10,20 @@ describe('#LowfareParser', () => {
   it('shold parse response without throwing', () => {
     lowfareRes.forEach((lowfare) => {
       expect(() => lowfareParser(lowfare)).to.not.throw();
+    });
+  });
+
+  it('should return correct data structure', () => {
+    lowfareRes.forEach((lowfare) => {
+      const parsed = lowfareParser(lowfare);
+      expect(parsed).to.not.be.empty;
+      expect(parsed.traceId).to.not.be.empty;
+      expect(parsed.transactionId).to.not.be.empty;
+      expect(parsed.distanceUnits).to.not.be.empty;
+      expect(parsed.currency).to.not.be.empty;
+      expect(parsed.responseTime).to.not.be.empty;
+      expect(parsed.solutions).to.not.be.empty;
+      expect(parsed.solutions).to.be.an.instanceOf(Array);
     });
   });
 });
